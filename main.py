19531d2b4f82b3d14e05c3b48c53d26b68d05553
mston3rob <shrekkamnev@gmail.com>
2022-12-27 17:28:12 +0300

    add load_image function

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import pygame
+import os
+import sys
 
+# инициалитзация pygame для работы со спрайтами и загрузкой изображения
+pygame.init()
+size = width, height = 1200, 800
+screen = pygame.display.set_mode(size)
 
+# функция загрузки изоражения из папки data
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    # конвертирование изображения для обрезки фона
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+# основной игровой цикл
 if __name__ == '__main__':
-    pygame.init()
-    size = width, height = 1200, 800
-    screen = pygame.display.set_mode(size)
-
     running = True
     while running:
 
