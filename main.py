2803c4fc4615efed018dd97375bf4ab83bde769d
mston3rob <shrekkamnev@gmail.com>
2022-12-27 17:18:39 +0300

    adding game cycle as base

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-# testing git-repo connection
+import pygame
+
+
+if __name__ == '__main__':
+    pygame.init()
+    size = width, height = 1200, 800
+    screen = pygame.display.set_mode(size)
+
+    running = True
+    while running:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        pygame.display.flip()
+    pygame.quit()
