4c16cf53fa63977f2156fddd0db469213b785167
mston3rob <shrekkamnev@gmail.com>
2023-01-06 15:11:16 +0300

    added blitRotate(), update() redacted, new surface

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ meteorites = pygame.sprite.Group()
 # список из изображений метеоритов (длинна=9)
 images_of_meteorites = []
 FPS = 60
-
+surface = pygame.Surface((width, height))
 # функция загрузки изоражения из папки data
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
@@ -32,6 +32,20 @@ def load_image(name, colorkey=None):
     return image
 
 
+def blitRotate(surf, image, pos, originPos, angle):
+    image_rect = image.get_rect(topleft=(pos[0] - originPos[0], pos[1] - originPos[1]))
+    offset_center_to_pivot = pygame.math.Vector2(pos) - image_rect.center
+
+    rotated_offset = offset_center_to_pivot.rotate(-angle)
+
+    rotated_image_center = (pos[0] - rotated_offset.x, pos[1] - rotated_offset.y)
+
+    rotated_image = pygame.transform.rotate(image, angle)
+    rotated_image_rect = rotated_image.get_rect(center=rotated_image_center)
+
+    surf.blit(rotated_image, rotated_image_rect)
+
+
 class Meteorite(pygame.sprite.Sprite):
     for i in range(1, 10):
         images_of_meteorites.append(load_image(f'meteor{i}.png'))
@@ -41,6 +55,7 @@ class Meteorite(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(random.choice(images_of_meteorites), (100, 100))
         self.rect = self.image.get_rect()
         self.angle = 0
+        w = self.image.get_size()[0] / 2
         # стартовая пзиция
         self.rect.x = random.randint(0, width)
         self.rect.y = 0
@@ -48,13 +63,15 @@ class Meteorite(pygame.sprite.Sprite):
         self.vy = random.randint(3, 5)
         # перемещение методом rect.move() работает не покадрово а за каждый тик (1000 раз в секунду),
         # поэтому указываем скорось в пикселях, деленных на фреймрейт
-        self.vx = random.randint(-((self.rect.left / FPS) / ((height / FPS) / self.vy)) // 1, (((width / FPS) - (self.rect.right / FPS)) / ((height / FPS) / self.vy)) // 1)
+        self.vx = random.randint(-(((self.rect.left - w) / FPS) / ((height / FPS) / self.vy)) // 1, (((width / FPS) - ((self.rect.right - w) / FPS)) / ((height / FPS) / self.vy)) // 1)
 
     def update(self):
-        self.rect = self.rect.move(self.vx, self.vy)
-
-for i in range(5):
-    Meteorite(meteorites)
+        self.rect.x += self.vx
+        self.rect.y += self.vy
+        w, h = self.image.get_size()
+        blitRotate(surface, self.image, (self.rect.x, self.rect.y), (w / 2, h / 2), self.angle)
+        screen.blit(surface, (0, 0))
+        self.angle += 1
 
 
 # основной игровой цикл
@@ -69,6 +86,7 @@ if __name__ == '__main__':
         screen.fill(pygame.Color('Black'))
         meteorites.draw(screen)
         meteorites.update()
+        surface.fill(pygame.Color('Black'))
         clock.tick(FPS)
         pygame.display.flip()
     pygame.quit()
